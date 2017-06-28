68adbb85a37928fe23c1323b5f62f2f0a00c1d4f
NitrateOrigins <templex.mc@gmail.com>
2017-06-27 23:09:12 -0400

    Added more files.

--- src/main/java/net/ddns/templex/TemplexAdditionsPlugin.java
+++ src/main/java/net/ddns/templex/TemplexAdditionsPlugin.java
@@ -3,10 +3,11 @@ package net.ddns.templex;
 import io.github.trulyfree.va.command.commands.TabbableCommand;
 import io.github.trulyfree.va.config.ConfigHandler;
 import lombok.Getter;
-import net.ddns.templex.commands.*;
-import net.ddns.templex.commands.home.HomeCommand;
+import net.ddns.templex.commands.HelloCommand;
+import net.ddns.templex.commands.ListCommand;
+import net.ddns.templex.commands.SurvivalCommand;
+import net.ddns.templex.commands.TPACommand;
 import net.ddns.templex.commands.home.HomeHandler;
-import net.ddns.templex.commands.home.SetHomeCommand;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class TemplexAdditionsPlugin extends Plugin {
@@ -25,6 +26,7 @@ public class TemplexAdditionsPlugin extends Plugin {
                 new HelloCommand(),
                 new TPACommand(),
                 new SurvivalCommand(),
+                new ListCommand(),
                 //new SpawnCommand(this),
                 //new RTPCommand(this),
                 //new HomeCommand(homeHandler), // TODO
---
+++ src/main/java/net/ddns/templex/commands/PingCommand.java
@@ -0,0 +1,55 @@
+package net.ddns.templex.commands;
+
+import io.github.trulyfree.va.command.commands.TabbableCommand;
+import io.github.trulyfree.va.daemon.Daemon;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.event.TabCompleteEvent;
+
+import java.util.Collections;
+
+/**
+ * Allows players to ping staff (or Nitrate1 specifically) if they need help.
+ */
+public class PingCommand extends TabbableCommand {
+
+    public PingCommand() {  super("ping","templex.ping");    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+        if (strings[1] == null) {
+            strings[1] = "owner";
+        }
+        if (strings[1] == "owner") {
+            try {
+                commandSender.sendMessage(new ComponentBuilder("Pong! Just pinged the owner!").color(ChatColor.BLUE).create());
+                Daemon.getInstance().submitCommands(Collections.singletonList("/execute @a[team=Owner] ~ ~ ~ playsound minecraft:block.note.flute master @s"));
+                wait(1);
+                Daemon.getInstance().submitCommands(Collections.singletonList("/execute @a[team=Owner] ~ ~ ~ playsound minecraft:block.note.flute master @s"));
+                wait(1);
+                Daemon.getInstance().submitCommands(Collections.singletonList("/execute @a[team=Owner] ~ ~ ~ playsound minecraft:block.note.flute master @s"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (strings[1] == "staff") {
+            try {
+                commandSender.sendMessage(new ComponentBuilder("Pong! Just pinged all staff!").color(ChatColor.BLUE).create());
+                Daemon.getInstance().submitCommands(Collections.singletonList("/execute @a[tag=OP] ~ ~ ~ playsound minecraft:block.note.flute master @s"));
+                wait(1);
+                Daemon.getInstance().submitCommands(Collections.singletonList("/execute @a[tag=OP] ~ ~ ~ playsound minecraft:block.note.flute master @s"));
+                wait(1);
+                Daemon.getInstance().submitCommands(Collections.singletonList("/execute @a[tag=OP] ~ ~ ~ playsound minecraft:block.note.flute master @s"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            commandSender.sendMessage(new ComponentBuilder("Incorrect usage!").color(ChatColor.RED).create());
+            commandSender.sendMessage(new ComponentBuilder("Usage: /ping <staff | owner>").color(ChatColor.RED).create()); // More teams to be added at a later date.
+        }
+    }
+
+    @Override
+    public void handleTabCompleteEvent(TabCompleteEvent event) {    Util.pushAutocompletePlayers(event);    }
+}
