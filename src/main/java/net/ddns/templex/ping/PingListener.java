191352816c673709e787c26b1dd028a27932ad3c
vtcakavsmoace <vtcakavsmoace@gmail.com>
2017-07-14 21:58:08 -0500

    Fix MOTD

--- src/main/java/net/ddns/templex/ping/PingListener.java
+++ src/main/java/net/ddns/templex/ping/PingListener.java
@@ -1,7 +1,6 @@
 package net.ddns.templex.ping;
 
 import net.md_5.bungee.api.ServerPing;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -10,11 +9,10 @@ import net.md_5.bungee.event.EventPriority;
 public class PingListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
+    @SuppressWarnings("deprecation")
     public void onProxyPingEvent(ProxyPingEvent event) {
         ServerPing ping = event.getResponse();
-        ping.setDescriptionComponent(
-                new TextComponent("Templex: Where snapshot innovation is forefront.")
-        );
+        ping.setDescription("§7>> §3Templex §7<< §7- §6Powered by commands §8- §8[§41§7.§412§7.§4x§8]\n§4Hard §7§lVanilla §bSurvival §7- §b§o[Completely Vanilla]");
     }
 
 }
